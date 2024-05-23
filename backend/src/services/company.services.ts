c16c0fa711120d5543a15c703bf2a15c89fc45b0
Welinton Suarez <wesgdeveloper@gmail.com>
2024-05-23 08:48:06 -0500

    Add company router and services

---
+++ backend/src/services/company.services.ts
@@ -0,0 +1,65 @@
+import { Empresa } from "@prisma/client";
+import { prisma } from "../config/prisma";
+import { HttpException } from "../utils/HttpException";
+import { CODE } from "../utils/constants";
+
+class CompanyServices {
+  public createCompany = async (data: Empresa) => {
+    try {
+      const newCompany = await prisma.empresa.create({
+        data,
+        select: { id: true, name: true, ruc: true },
+      });
+      return newCompany;
+    } catch (error) {
+      throw new HttpException(CODE.BAD_REQUEST, "No se pudo crear la empresa");
+    }
+  };
+
+  public getCompanies = async () => {
+    try {
+      const companies = await prisma.empresa.findMany({
+        select: { id: true, name: true, ruc: true },
+      });
+      return companies;
+    } catch (error) {
+      throw new HttpException(
+        CODE.BAD_REQUEST,
+        "No se pudo obtener las empresas"
+      );
+    }
+  };
+
+  public getCompany = async (id: string) => {
+    try {
+      const company = await prisma.empresa.findUnique({
+        where: { id },
+        select: { id: true, name: true, ruc: true },
+      });
+      return company;
+    } catch (error) {
+      throw new HttpException(
+        CODE.BAD_REQUEST,
+        "No se pudo obtener la empresa"
+      );
+    }
+  };
+
+  public updateCompany = async (id: string, data: Empresa) => {
+    try {
+      const company = await prisma.empresa.update({
+        where: { id },
+        data,
+        select: { id: true, name: true, ruc: true },
+      });
+      return company;
+    } catch (error) {
+      throw new HttpException(
+        CODE.BAD_REQUEST,
+        "No se pudo actualizar la empresa"
+      );
+    }
+  };
+}
+
+export default new CompanyServices();
