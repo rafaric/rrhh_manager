e4d0805080809e87b6ff24b943e51f68643a5c0a
LautaroLD <duranlautarogabriel@gmail.com>
2024-05-25 23:03:52 -0300

    fix style in register page

--- frontend/src/routes/auth/registro/index.tsx
+++ frontend/src/routes/auth/registro/index.tsx
@@ -5,11 +5,11 @@ import { RegisterForm } from "~/components/RegisterForm";
 export default component$(() => {
   return (
     <section class="m-auto grid min-h-svh w-screen grid-rows-2">
-      <div class="m-auto flex w-full max-w-xs flex-grow flex-col gap-3">
+      <div class="m-auto  mb-0 flex w-full max-w-xs flex-grow flex-col gap-3">
         <h1 class="text-3xl font-semibold">Bienvenido</h1>
         <RegisterForm />
       </div>
-      <div class="text-center">
+      <div class="pt-8 text-center">
         <p>
           Ya tienes cuenta?
           <Link class="text-primary" href="/auth/iniciar-sesion/">
