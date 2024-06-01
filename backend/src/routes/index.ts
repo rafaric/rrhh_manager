06c51285922280accf4371c2d5c4af3f15f9ad85
MartinPC <MartinChrisPC@hotmail.com>
2024-06-01 18:21:39 -0500

    ADD cargo route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from "./user.router";
 import companyRouter from "./company.router";
+import cargoRouter from "./cargo.router";
 
 const router: Router = Router();
 
 router.use("/users", userRouter);
 router.use("/companys", companyRouter);
+router.use("/cargo",cargoRouter)
 
 export default router;
