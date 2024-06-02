1157d77feee259b9dfd71b648ed49961b795ff32
MartinPC <MartinChrisPC@hotmail.com>
2024-06-02 17:54:00 -0500

    ADD update User

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
-import { errorMessage } from "../utils/HandleError";
-import UserService from "../services/user.service";
-import { CODE } from "../utils/constants";
+import { Request, Response } from 'express';
+import { errorMessage } from '../utils/HandleError';
+import UserService from '../services/user.service';
+import { CODE } from '../utils/constants';
 
 class UserController {
 	public register = async (req: Request, res: Response) => {
@@ -10,7 +10,7 @@ class UserController {
 			const response = await UserService.createUser(body);
 			res
 				.status(CODE.CREATED)
-				.json({ data: response, message: "Usuario Creado Correctamente" });
+				.json({ data: response, message: 'Usuario Creado Correctamente' });
 		} catch (error) {
 			errorMessage(res, error);
 		}
@@ -22,7 +22,7 @@ class UserController {
 			const token = await UserService.login(body);
 			res
 				.status(CODE.ACCEPTED)
-				.json({ token, message: "Logeado Correctamente" });
+				.json({ token, message: 'Logeado Correctamente' });
 		} catch (error) {
 			errorMessage(res, error);
 		}
@@ -33,6 +33,23 @@ class UserController {
 			const id = String(req.user?.id);
 			const user = await UserService.getById(id);
 			res.status(CODE.OK).json(user);
+		} catch (error) {
+			errorMessage(res, error);
+		}
+	};
+
+	public update = async (req: Request, res: Response) => {
+		try {
+			const { id } = req.params;
+			const { body } = req;
+			const updatedUser = await UserService.update(id!, body);
+
+			res
+				.status(CODE.ACCEPTED)
+				.json({
+					data: updatedUser,
+					message: 'Usuario Actualizado correctamente'
+				});
 		} catch (error) {
 			errorMessage(res, error);
 		}
--- backend/src/types/user.ts
+++ backend/src/types/user.ts
@@ -1,13 +1,30 @@
-import { Usuario } from "@prisma/client";
+import { Usuario } from '@prisma/client';
 
 export type AuthData = Pick<
 	Usuario,
-	"email" | "password" | "nombre" | "apellido" | "dni"
+	'email' | 'password' | 'nombre' | 'apellido' | 'dni'
 >;
 
 export type UserResponse = Pick<
 	Usuario,
-	"id" | "email" | "nombre" | "apellido" | "dni" | "rol"
+	'id' | 'email' | 'nombre' | 'apellido' | 'dni' | 'rol'
 >;
 
-export type UserPayload = Pick<Usuario, "id" | "email" | "rol" | "dni">;
+export type UserPayload = Pick<Usuario, 'id' | 'email' | 'rol' | 'dni'>;
+
+export type UpdateData = Partial<
+	Pick<
+		Usuario,
+		| 'password'
+		| 'nombre'
+		| 'apellido'
+		| 'ciudad'
+		| 'codigo_postal'
+		| 'direccion'
+		| 'rol'
+		| 'empresa_id'
+		| 'telefono'
+		| 'dni'
+		| 'contrato_id'
+	>
+>;
