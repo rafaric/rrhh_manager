2b176d25162a2814a3b6289ae6501ebd1ee84ceb
MartinPC <MartinChrisPC@hotmail.com>
2024-06-03 13:43:57 -0500

    ADD exception for contract ACTIVO

--- backend/src/services/contract.service.ts
+++ backend/src/services/contract.service.ts
@@ -2,12 +2,15 @@ import { prisma } from '../database';
 import { ContractData } from '../types/contract';
 import userService from './user.service';
 import cargoService from './cargo.service';
+import { HttpException } from '../utils/HttpException';
+import { CODE } from '../utils/constants';
 
 class ContractService {
 	public create = async (data: ContractData) => {
 		const usuarioFound = await userService.getById(data.usuarioId!);
 
-		if (usuarioFound.contrato) {
+		if (usuarioFound.contrato.length !== 0) {
+			throw new HttpException(CODE.BAD_REQUEST, 'Ya hay un contrato Activo');
 		}
 
 		const cargoFound = await cargoService.getById(data.cargoId!);
