8a7632af52ad5bb4e267ed43830cab8e559b4846
Welinton Suarez <wesgdeveloper@gmail.com>
2024-06-06 18:01:01 -0500

    update route paths to use consistent naming conventions

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,16 +1,16 @@
-import { Router } from 'express';
-import userRouter from './user.router';
-import companyRouter from './company.router';
-import cargoRouter from './cargo.router';
-import licenseApplicationRouter from './licenseAplication.router';
-import contractRouter from './contract.router';
+import { Router } from "express";
+import userRouter from "./user.router";
+import companyRouter from "./company.router";
+import cargoRouter from "./cargo.router";
+import licenseApplicationRouter from "./licenseAplication.router";
+import contractRouter from "./contract.router";
 
 const router: Router = Router();
 
-router.use('/users', userRouter);
-router.use('/companys', companyRouter);
-router.use('/cargo', cargoRouter);
-router.use('licenseApplication', licenseApplicationRouter);
-router.use('/contract', contractRouter);
+router.use("/users", userRouter);
+router.use("/companys", companyRouter);
+router.use("/cargo", cargoRouter);
+router.use("/licenseAplication", licenseApplicationRouter);
+router.use("/contract", contractRouter);
 
 export default router;
